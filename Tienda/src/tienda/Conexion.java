9c92de8e4856d03f161176c7ae96f6eb50fc1829
mafedev <ladrillito966@gmail.com>
2025-04-26 23:34:41 +0200

    Código java agregado

---
+++ Tienda/src/tienda/Conexion.java
@@ -0,0 +1,17 @@
+package tienda;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Conexion {
+	
+	private String url = "jdbc:mysql://localhost:3306/tienda";
+	
+	static void conexionBdd() {
+		
+	}
+	
+}
