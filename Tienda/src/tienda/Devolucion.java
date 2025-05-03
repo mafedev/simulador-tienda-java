6c010f45b1fa9a4b5e0a0bace5cac72792ae4f6e
mafedev <ladrillito966@gmail.com>
2025-05-03 16:17:52 +0200

    Comentado y con atributos

--- Tienda/src/tienda/Devolucion.java
+++ Tienda/src/tienda/Devolucion.java
@@ -7,10 +7,67 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Devolucion {
-
+	private int id;
+	private int ventaId;
+	private int productoId;
+	private String motivo;
+	private double dineroDevuelto;
+	
+	// --------------------- Constructores ---------------------
 	public Devolucion() {
 	}
 
+	public Devolucion(int id, int ventaId, int productoId, String motivo, double dineroDevuelto) {
+		this.id = id;
+		this.ventaId = ventaId;
+		this.productoId = productoId;
+		this.motivo = motivo;
+		this.dineroDevuelto = dineroDevuelto;
+	}
+
+	// --------------------- Getters y Setters ---------------------
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getVentaId() {
+		return ventaId;
+	}
+
+	public void setVentaId(int ventaId) {
+		this.ventaId = ventaId;
+	}
+
+	public int getProductoId() {
+		return productoId;
+	}
+
+	public void setProductoId(int productoId) {
+		this.productoId = productoId;
+	}
+
+	public String getMotivo() {
+		return motivo;
+	}
+
+	public void setMotivo(String motivo) {
+		this.motivo = motivo;
+	}
+
+	public double getDineroDevuelto() {
+		return dineroDevuelto;
+	}
+
+	public void setDineroDevuelto(double dineroDevuelto) {
+		this.dineroDevuelto = dineroDevuelto;
+	}
+	
+	// --------------------- Métodos ---------------------
+	// Registra en la tabla devoluciones la devolución con los datos que recibe como parámetro
 	public static void registrarDevolucion(Connection c, int ventaId, int productoId, String motivo, double dineroDevuelto) {
 		try {
 			String insertarDevolucion = "INSERT INTO devoluciones (venta_id, producto_id, motivo, dinero_devuelto) VALUES (?, ?, ?, ?)";
@@ -24,11 +81,12 @@ public class Devolucion {
 			e.printStackTrace();
 		}
 	}
-	
+
+	// Muestra todas las devoluciones registradas
 	public static void mostrarDevoluciones(Connection c) {
 		try {
 			Statement s = c.createStatement();
-			ResultSet rs = s.executeQuery("SELECT * FROM devoluciones");
+			ResultSet rs = s.executeQuery("SELECT * FROM devoluciones"); // Muestra todo lo de la tabla devoluciones
 			
 			System.out.println("  ┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑");
 			System.out.println("  │           DEVOLUCIONES");
